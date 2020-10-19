a0d0f9c36d6ee02b6874855a16b3664527548988
Sastry <pssastry@a483e77984df.ant.amazon.com>
2020-10-19 17:00:33 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const config = require('./config.json');
+
+export class App extends Component {
+
+    state = {
+        inventory: []
+    }
+
+    fetchInventory = async () => {
+        try {
+            const res = await axios.get(`${config.api.invokeUrl}/inventory`);
+            console.log(res)
+            this.setState({ inventory: res.data })
+          } catch(err) {
+            console.log("Error: ", err)
+          }
+    }
+
+    componentDidMount() {
+        this.fetchInventory();
+    }
+
+    render() {
+        const renderedData = this.state.inventory.map((data, index) => {
+            return (
+                <div key={index}>
+                    Server: {data.server_name} 
+                    Application: {data.app_name}
+                </div>)
+        })
+        return (
+            <div>
+                App
+                {renderedData}
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
